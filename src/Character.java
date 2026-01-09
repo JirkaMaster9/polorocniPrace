a9503e92b7c6795cebe51bf95a5409a73859fe28
jirka <jirkakolinek4@gmail.com>
2026-01-09 10:49:09 +0100

    created class character and heads of methods for it

---
+++ src/Character.java
@@ -0,0 +1,13 @@
+import Item.Item;
+import Rune.Rune;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Character {
+    private String name;
+    private int health,maxHealth,mana,maxMana;
+    private List <Item> itemList;
+    private ArrayList <Rune> runeList;
+
+}
