89f082e11ad31ae5ae94e586030fc67991536781
jirka <jirkakolinek4@gmail.com>
2026-01-22 16:44:01 +0100

    created consol with 2/1 text inputs and added hel

--- src/Command/Console.java
+++ src/Command/Console.java
@@ -1,4 +1,46 @@
 package Command;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Console {
-}
+    private HashMap<String, Command> commands;
+    private Scanner scanner;
+
+    public Console(){
+
+        scanner = new Scanner(System.in);
+        commands = new HashMap<>();
+    }
+    public void inicializator(){
+        commands.put("help",new Help());
+
+    }
+    public void execute() {
+        System.out.print(" ~ > ");
+        String command = scanner.nextLine();
+
+
+        String[] cm = command.trim().toLowerCase().split(" ");
+        if(commands.containsKey(cm[0])) {
+            Command com = commands.get(cm[0]);
+            if (cm.length>2){
+                System.out.println("too many parameters");
+                return;
+            }
+            if (cm.length !=1) {
+                System.out.println(com.execute(cm[1]));
+//              isExit = commands.get(command).exit();
+            }else{
+                System.out.println(com.execute(""));
+            }
+
+        }else {
+        System.out.println("command not found");
+        }
+
+    }
+
+
+
+    }
