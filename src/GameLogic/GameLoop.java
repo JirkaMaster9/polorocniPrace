8562a2961b054a0c7d18a29b7de66b69d7a35c5d
jirka <jirkakolinek4@gmail.com>
2026-01-22 21:18:53 +0100

    created game loop

---
+++ src/GameLogic/GameLoop.java
@@ -0,0 +1,34 @@
+package GameLogic;
+
+import Command.Console;
+import Map.Map;
+import Map.Coordinates;
+import Map.TurboMap;
+
+import static GameLogic.JsonLoader.loadMap;
+
+public class GameLoop {
+    private boolean isExit = false;
+    public void start() {
+
+        Character character = new Character();
+        Coordinates cor = new Coordinates(1,2);
+        TurboMap turboloadedMap= JsonLoader.loadMap("resources/gamedata.json");
+        character.setCurrentLocation(turboloadedMap.getMaps().getFirst().getInteractSpotMap().get(cor));
+        character.setMap(turboloadedMap.getMaps().getFirst());
+        character.setCoordinates(cor);
+//        character.setMap();
+//        character.setCurrentLocation();
+//        character.setCoordinates();
+
+
+        Console console = new Console(character);
+        console.inicializator();
+
+        do {
+            console.execute();
+
+        } while (!isExit);
+    }
+
+}
